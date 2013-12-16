c405c68c3cc5be807f417dc2ab62e06e649c484b
Thomas Husmann <thusmann@gmail.com>
2013-12-16 21:43:17 +0100

    fixed indent

--- temperatur.py
+++ temperatur.py
@@ -27,24 +27,24 @@ def read_temp():
 	while(True):
 	# Run the DHT program to get the humidity and temperature readings!
 
-	output = subprocess.check_output(["~/Adafruit-Raspberry-Pi-Python-Code/Adafruit_DHT_Driver/Adafruit_DHT", "11", "4"]);
-	print output
-	matches = re.search("Temp =\s+([0-9.]+)", output)
-	if (not matches):
-		time.sleep(3)
-		continue
-	temp = float(matches.group(1))
+		output = subprocess.check_output(["~/Adafruit-Raspberry-Pi-Python-Code/Adafruit_DHT_Driver/Adafruit_DHT", "11", "4"]);
+		print output
+		matches = re.search("Temp =\s+([0-9.]+)", output)
+		if (not matches):
+			time.sleep(3)
+			continue
+		temp = float(matches.group(1))
   
-	# search for humidity printout
-	matches = re.search("Hum =\s+([0-9.]+)", output)
-	if (not matches):
-		time.sleep(3)
-		continue
-	humidity = float(matches.group(1))
+		# search for humidity printout
+		matches = re.search("Hum =\s+([0-9.]+)", output)
+		if (not matches):
+			time.sleep(3)
+			continue
+		humidity = float(matches.group(1))
 
-	print "Temperature: %.1f C" % temp
-	print "Humidity:    %.1f %%" % humidity
-	return temp
+		print "Temperature: %.1f C" % temp
+		print "Humidity:    %.1f %%" % humidity
+		return temp
 
  
 # function to return a datastream object. This either creates a new datastream,
